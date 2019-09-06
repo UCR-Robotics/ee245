1ab8506785c0827d83d9891ff497797aa885f0f9
Hanzhe Teng <hteng007@ucr.edu>
2019-09-06 16:26:00 -0700

    for vicon odom mocap

--- scripts/publish_external_position_vicon_odom.py
+++ scripts/publish_external_position_vicon_odom.py
@@ -2,37 +2,37 @@
 
 import rospy
 import tf
-from geometry_msgs.msg import PointStamped, TransformStamped, PoseStamped
+from geometry_msgs.msg import PointStamped
+from nav_msgs.msg import Odometry
 from crazyflie_driver.srv import UpdateParams
 
-def onNewTransform(pose):
+def onNewTransform(odom):
     global msg
     global pub
     global firstTransform
 
     if firstTransform:
         # initialize kalman filter
-        rospy.set_param("kalman/initialX", pose.pose.position.x)
-        rospy.set_param("kalman/initialY", pose.pose.position.y)
-        rospy.set_param("kalman/initialZ", pose.pose.position.z)
+        rospy.set_param("kalman/initialX", odom.pose.pose.position.x)
+        rospy.set_param("kalman/initialY", odom.pose.pose.position.y)
+        rospy.set_param("kalman/initialZ", odom.pose.pose.position.z)
         update_params(["kalman/initialX", "kalman/initialY", "kalman/initialZ"])
-
         rospy.set_param("kalman/resetEstimation", 1)
-        update_params(["kalman/resetEstimation"]) 
+        update_params(["kalman/resetEstimation"])
         firstTransform = False
     else:
-        msg.header.frame_id = pose.header.frame_id
-        msg.header.stamp = pose.header.stamp
+        msg.header.frame_id = odom.header.frame_id
+        msg.header.stamp = odom.header.stamp
         msg.header.seq += 1
-        msg.point.x = pose.pose.position.x
-        msg.point.y = pose.pose.position.y
-        msg.point.z = pose.pose.position.z
+        msg.point.x = odom.pose.pose.position.x
+        msg.point.y = odom.pose.pose.position.y
+        msg.point.z = odom.pose.pose.position.z
         pub.publish(msg)
 
 
 if __name__ == '__main__':
-    rospy.init_node('publish_external_position_optitrack', anonymous=True)
-    topic = rospy.get_param("~topic", "/optitrack/cf1/pose")
+    rospy.init_node('publish_external_position_vicon', anonymous=True)
+    topic = rospy.get_param("~topic", "/vicon/cf/cf")
 
     rospy.wait_for_service('update_params')
     rospy.loginfo("found update_params service")
@@ -45,6 +45,6 @@ if __name__ == '__main__':
     msg.header.stamp = rospy.Time.now()
 
     pub = rospy.Publisher("external_position", PointStamped, queue_size=1)
-    rospy.Subscriber(topic, PoseStamped, onNewTransform)
+    rospy.Subscriber(topic, Odometry, onNewTransform)
 
     rospy.spin()
